cfcea7a5923ecd01fb10b8c54c1e2f64fbc088fa
Jose <josemanuelguerreroduarte@gmail.com>
2021-07-01 07:02:02 -0500

    first commit

---
+++ resources/views/misc/acerca_de.blade.php
@@ -0,0 +1,42 @@
+@extends("maestra")
+@section("titulo","Acerca de")
+@section("contenido")
+    <div class="row">
+        <div class="col-12">
+            <h1>Acerca de</h1>
+            <p>
+                Programado por <a href="//parzibyte.me/blog">Jose Manuel Guerrero Duarte</a>
+            </p>
+            <pre>
+            </pre>
+            <h2>Créditos de las imágenes</h2>
+
+            <strong>Productos:&nbsp;</strong>Icons made by <a href="https://www.flaticon.com/authors/icongeek26"
+                                                              title="Icongeek26">Icongeek26</a> from
+            <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            <br>
+            <strong>Vender:&nbsp;</strong>Icons made by <a href="https://www.flaticon.com/authors/freepik"
+                                                           title="Freepik">Freepik</a>
+            from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            <br>
+            <strong>Ventas:&nbsp;</strong>Icons made by <a href="https://www.flaticon.com/authors/eucalyp"
+                                                           title="Eucalyp">Eucalyp</a>
+            from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            <br>
+            <strong>Soporte:&nbsp;</strong>Icons made by <a href="https://www.flaticon.com/authors/ddara"
+                                                            title="dDara">dDara</a> from <a
+                href="https://www.flaticon.com/"
+                title="Flaticon">
+                www.flaticon.com</a>
+            <br>
+            <strong>Acerca de:&nbsp;</strong>Icons made by <a href="https://www.flaticon.com/authors/inipagistudio"
+                                                              title="inipagistudio">inipagistudio</a> from <a
+                href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                <br>
+                <strong>Usuarios:</strong> Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" rel="nofollow">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" rel="nofollow">www.flaticon.com</a>
+                <br>
+                <strong>Clientes:</strong> Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik" rel="nofollow">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon" rel="nofollow">www.flaticon.com</a>
+                <br>
+        </div>
+    </div>
+@endsection
